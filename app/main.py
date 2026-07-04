c7d5a078927b67575d86ccd36f0b128cef87d4f7
Kamaresh154 <kamaresh154@users.noreply.github.com>
2026-07-04 12:44:19 +0530

    fix: allow all CORS origins for Android WebView compatibility

--- app/main.py
+++ app/main.py
@@ -49,8 +49,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origin_list,
-    allow_origin_regex=r"^(capacitor://localhost|http://localhost(:\d+)?)$",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
